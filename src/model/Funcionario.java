8c4b215613ed6d4dc338ada8678deaaac1529e1f
Cássio <142359405+CassioJr-Dev@users.noreply.github.com>
2024-11-27 15:57:32 -0300

    Update Funcionario.java

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,78 +1,19 @@
 package model;
 
-public class Funcionario {
-    private int id;
-    private String nome;
-    private String cpf;
+public class Funcionario extends Usuario {
     private String cargo;
-    private String senha;
-    private String telefone; // Novo campo telefone
-    private String endereco; // Novo campo endereço
 
-    public Funcionario(int id, String nome, String cpf, String cargo, String senha, String telefone, String endereco) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
+    public Funcionario(String nome, String cpf, String cargo, String telefone, String endereco, String senha) {
+        super(nome, cpf, telefone, endereco, senha);
         this.cargo = cargo;
-        this.senha = senha;
-        this.telefone = telefone; // Atribuindo telefone
-        this.endereco = endereco; // Atribuindo endereço
     }
 
-    // Getters e Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
+    // Getter e Setter
     public String getCargo() {
         return cargo;
     }
 
     public void setCargo(String cargo) {
         this.cargo = cargo;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getTelefone() {
-        return telefone;  // Adicionando getter para telefone
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;  // Adicionando setter para telefone
-    }
-
-    public String getEndereco() {
-        return endereco;  // Adicionando getter para endereco
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;  // Adicionando setter para endereco
     }
 }
